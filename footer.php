4a9525f2762f7464fe70ed5447ab181c6efe8e37
chang96min <ckdals8785@naver.com>
2021-05-03 17:15:11 +0900

    2021.05.03

---
+++ footer.php
@@ -0,0 +1,16 @@
+   <!--하단 정보-->
+   <div class="footer">
+       <div>
+           <div class="f-logo">
+               <h1><a href="../index.html"><img src="../img/logo.png" alt=""><span>기타의 시작은 C코드에서부터</span></a></h1>
+           </div>
+           <div class="f-copy">
+               <p>copyright(c) 기타의 시작은 C코드에서부터. All Rights Reserved</p>
+           </div>
+       </div>
+
+   </div>
+   <script>
+AOS.init();
+   </script>
+   </body>
